c9afca9937b00466e63aff710bf1aab20fce909f
Durga Dinakaran <durga.dinakaran@thoughtworks.com>
2022-06-29 12:57:26 +0530

    Add. Adds Header using layout component

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import './App.css';
+import Layout from './layout/Layout';
 
 const App = () => {
     axios(`http://localhost:8080/api/v1/books`).then(books => {
         console.log(books);
     });
-    return <div>My First Component</div>
+    return (
+        <Layout>
+            <div>My First Component</div>
+        </Layout>
+    )
 };
 
 export default App;
---
+++ src/component/layout/Layout.js
@@ -0,0 +1,22 @@
+import { Box } from "@mui/system"
+import Header from "./Header";
+import PropTypes from "prop-types"
+
+const propTypes = {
+    children: PropTypes.node.isRequired
+}
+const Layout = ({ children }) => {
+    return (
+        <Box>
+            <Box>
+                <Header/>
+            </Box>
+            <Box mt={8} ml={5}>
+                {children}
+            </Box>
+        </Box>
+    )
+}
+
+Layout.propTypes = propTypes;
+export default Layout;
